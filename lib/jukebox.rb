7a8a314a020fbd1d0b2f4b982c59ea6d8b7b42fe
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-28 17:30:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -40,3 +40,22 @@ def exit_jukebox
   puts "Goodbye"
 end
 
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
+end
